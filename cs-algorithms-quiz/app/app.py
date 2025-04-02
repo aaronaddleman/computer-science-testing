4e0823ccfc0971a8a793ff7d8e506c53012ba8a2
Aaron Addleman <aaronaddleman@gmail.com>
2025-04-02 16:42:42 -0700

    updated list, added tests

--- cs-algorithms-quiz/app/app.py
+++ cs-algorithms-quiz/app/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/algo/binary-search-tree/info')
+def binary_search_tree_info():
+    return render_template('binary_search_tree_info.html')
+
+@app.route('/algo/binary-search-tree/test')
+def binary_search_tree_test():
+    return render_template('binary_search_tree_test.html')
+
 if __name__ == '__main__':
     app.run(debug=True) 
